710cca9d5ca7fbfee673276a46a4c7841d339a1b
Osa Isacson <asaisacson@gmail.com>
2019-11-11 00:37:03 +0100

    add flow app > city > postcode > rep

--- client/src/components/City.js
+++ client/src/components/City.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import Postcode from "./Postcode";
+import React, { Component } from 'react';
+import Postcode from './Postcode';
 
 export default class City extends Component {
   constructor(props) {
@@ -10,12 +10,18 @@ export default class City extends Component {
 
   render() {
     let cityBackgroundImage =
-      "https://images.unsplash.com/photo-1529655683826-aba9b3e77383?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80";
+      'https://images.unsplash.com/photo-1529655683826-aba9b3e77383?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9';
     // Placeholder image for now. Add a function here that connects to unsplash API and pulls in an img url that matches this.props.city.
 
     return (
       <>
-        <div class="center">
+        <div class="flex-center-column">
+          <input
+            className="text-center"
+            type="text"
+            value=""
+            placeholder="City"
+          ></input>
           <h1>Great, you're in {this.props.city}</h1>
           <img alt="city" className="city" src={cityBackgroundImage} />
           <h1>Now what's your postcode</h1>
--- client/src/components/Postcode.js
+++ client/src/components/Postcode.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Rep from "./Rep";
 
 export default class Postcode extends Component {
   constructor(props) {
@@ -8,8 +9,26 @@ export default class Postcode extends Component {
   }
 
   render() {
+    let postcodeCheck = true;
+    // True for now. Add a function here that checks if the postcode is valid. If so, return true and trigger repData.
+
+    let repData = null;
+    // Add a function here that checks if the postcode matches a rep found in the gov data. If so, set this.state.rep to that data.
+
     return (
-      <div className="Postcode">This is where the postcode field goes</div>
+      <>
+        <input
+          className="text-center"
+          type="text"
+          value=""
+          placeholder="Postcode"
+        ></input>
+        {postcodeCheck ? (
+          <Rep rep={repData} />
+        ) : (
+          <h3>Hey your postcode does not seem to exist. Try again.</h3>
+        )}
+      </>
     );
   }
 }
