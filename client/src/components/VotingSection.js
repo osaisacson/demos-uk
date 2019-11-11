2bfbb5df6790e215d1d25348e9dc6d1c5146ad59
Osa Isacson <asaisacson@gmail.com>
2019-11-11 23:49:48 +0100

    styles for voting section

--- client/src/components/VotingSection.js
+++ client/src/components/VotingSection.js
@@ -14,12 +14,12 @@ export default class VotingSection extends Component {
         <h2>{this.props.title}</h2>
         {this.props.category.map(item => {
           return (
-            <>
+            <div className="voting-section">
               <div key={item.id}>
                 <p>{item.text}</p>
               </div>
               <YourVote />
-            </>
+            </div>
           );
         })}
       </div>
